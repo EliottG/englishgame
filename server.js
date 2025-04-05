2676659104cfd06c18fa6c8968b5307b8cf93c7f
EliottG <eliott.geoffroy@gmail.com>
2025-04-05 15:26:13 +0200

    commit

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-const stringSimilarity = require('string-similarity');
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const bodyParser = require('body-parser');
 const { Pool } = require('pg');
 const app = express();
 const port = 3001;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DB_NAME,
+  password: process.env.PASSWORD,
+  port: process.env.PORT
 });
 
 app.use(cors());
@@ -21,28 +24,8 @@ app.get('/api/phrase', async (req, res) => {
   res.json(result.rows[0]);
 });
 
-const normalizeText = (text) => {
-  return text
-    .toLowerCase()
-    .replace(/[’'"]/g, '')         
-    .replace(/[^a-z0-9\s]/gi, '') 
-    .replace(/\s+/g, ' ')
-    .trim();
-};
 
-app.post('/api/verify', (req, res) => {
-  const { original, userInput } = req.body;
-
-  const originalClean = normalizeText(original);
-  const inputClean = normalizeText(userInput);
-
-  const similarity = stringSimilarity.compareTwoStrings(originalClean, inputClean);
-
-  const isCorrect = similarity >= 0.90;
-  
-  res.json({ correct: isCorrect });
-});
 
 app.listen(port, () => {
-  console.log(`Server running ${process.env.CURRENT_DOMAIN}:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
